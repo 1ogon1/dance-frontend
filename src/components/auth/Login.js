7d361cdaaabaf634aa6db590b5212aa29231fdae
tireofthisplace <cristalaiz1337@gmail.com>
2023-04-28 12:38:44 +0300

    save data

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import './Login.css';
+import axios from "axios";
 
 
 export const Login = () => {
@@ -18,15 +19,41 @@ export const Login = () => {
         setPassword(e.target.value)
     }
 
+    const onLoginHandler = async (e) => {
+        e.preventDefault()
+        let user = {loginName : login};
+        console.log(user)
+
+        await axios.post("http://localhost:3001/login", user)
+            .then(result => console.log(result.data))
+            .catch(error => console.log(error))
+        setLogin('')
+        window.location = '/login'
+        debugger;
+
+    }
+
+    const onPasswordHandler = async (e) => {
+        e.preventDefault()
+        let passwords = {passwordName: password};
+        console.log(passwords)
+
+        await axios.post("http://localhost:3001/login", passwords)
+            .then(result => console.log(result.data))
+            .catch(error => console.log(error))
+        setPassword('')
+        window.location = '/login'
+    }
+
     return (
-        <div className="Loginization-block">
+        <div className="loginization_block">
             <form>
                 <div>Login</div>
                 {(loginDirty && loginError) && <div style={{color: 'red'}}>{loginError}</div>}
-                <input onChange={e => loginHandler(e)} value={login} name="login" type="text"/>
+                <input onSubmit={onLoginHandler}  onChange={e => loginHandler(e)} value={login} name="login" type="text"/>
                 <div>Password</div>
                 {(passwordError && passwordDirty) && <div style={{color: 'red'}}>{passwordError}</div>}
-                <input onChange={e => passwordHandler(e)} value={password} name="password" type="password"/>
+                <input onSubmit={onPasswordHandler} onChange={e => passwordHandler(e)} value={password} name="password" type="password"/>
                 <button type='submit'>LOG IN</button>
             </form>
         </div>
