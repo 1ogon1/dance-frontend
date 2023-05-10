fab0e8194494ee1cb858468724459dc997717215
tireofthisplace <cristalaiz1337@gmail.com>
2023-05-10 10:37:32 +0300

    rewriten login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,58 +4,36 @@ import axios from "axios";
 
 
 export const Login = () => {
-    const [login, setLogin] = useState('')
-    const [password, setPassword] = useState('')
-    const [loginDirty, setLoginDirty] = useState('')
-    const [passwordDirty, setPasswordDirty] = useState('')
-    const [loginError, setLoginError] = useState('Wrong login!')
-    const [passwordError, setPasswordError] = useState('Wrong password!')
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleUsernameChange = (event) => setUserName(event.target.value);
+    const handlePasswordChange = (event) => setPassword(event.target.value);
 
-    const loginHandler = (e) => { // проверка логина
-        setLogin(e.target.value)
-    }
-    const passwordHandler = (e) => { // проверка пароля
-        setPassword(e.target.value)
-    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Отправляем запрос на сервер с данными для входа
+        console.log(`Username: ${username}, Password: ${password}`);
+        setUserName('');
+        setPassword('');
 
-    const onLoginHandler = async (e) => {
-        e.preventDefault()
-        let user = {loginName : login};
-        console.log(user)
-
-        await axios.post("http://localhost:3001/login", user)
-            .then(result => console.log(result.data))
-            .catch(error => console.log(error))
-        setLogin('')
-        window.location = '/login'
-        debugger;
-
-    }
-
-    const onPasswordHandler = async (e) => {
-        e.preventDefault()
-        let passwords = {passwordName: password};
-        console.log(passwords)
-
-        await axios.post("http://localhost:3001/login", passwords)
-            .then(result => console.log(result.data))
-            .catch(error => console.log(error))
-        setPassword('')
-        window.location = '/login'
-    }
+    };
 
     return (
-        <div className="loginization_block">
-            <form>
-                <div>Login</div>
-                {(loginDirty && loginError) && <div style={{color: 'red'}}>{loginError}</div>}
-                <input onSubmit={onLoginHandler}  onChange={e => loginHandler(e)} value={login} name="login" type="text"/>
-                <div>Password</div>
-                {(passwordError && passwordDirty) && <div style={{color: 'red'}}>{passwordError}</div>}
-                <input onSubmit={onPasswordHandler} onChange={e => passwordHandler(e)} value={password} name="password" type="password"/>
-                <button type='submit'>LOG IN</button>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Login:
+                    <input type="text" value={username} onChange={handleUsernameChange} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={handlePasswordChange} />
+                </label>
+                <br />
+                <button type="submit">Log In</button>
             </form>
         </div>
-    )
+    );
 }
