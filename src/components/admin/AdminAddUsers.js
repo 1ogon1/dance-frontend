9c7c1ecec2989007383e2f15adb7387f20748d51
D1m30 <petrenko_dv@ukr.net>
2023-05-16 19:20:38 +0300

    Добавлена папка style(общие стили)

--- src/components/admin/AdminAddUsers.js
+++ src/components/admin/AdminAddUsers.js
@@ -1,38 +1,45 @@
 import React from "react"
 import { Container } from "components/shared/container/Container"
+import  'components/admin/AdminAddUsers.css';
+import  'components/style/reset.css';
+import  'components/style/common.css';
 export const AdminAddUsers = () => {
     return (
-        <Container>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                <h1>Top 16</h1>
-                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" , padding: "41px", gap: "110px" }}>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                        {[...Array(6)].map((_, i) => (
-                            <div key={i}>
-                                <label>{`Dancer ${i + 1}`}</label>
-                                <input type="text" placeholder={`Participant ${i + 1}`} style={{ marginBottom: 10 }} />
+        <div className="admin-top bg">
+            <Container >
+         
+                    <div className="admin-top__container">
+                        <h1 className="title">Top 16</h1>
+                        <div className="admin-top__columns">
+                            <div  className="admin-top__column top-column">
+                                {[...Array(6)].map((_, i) => (
+                                    <div className="top-column__block input-block" key={i}>
+                                        <label className="top-column__title input-title">{`Dancer ${i + 1}`}</label>
+                                        <input className="top-column__input input-info" type="text" />
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                        {[...Array(6)].map((_, i) => (
-                            <div key={i + 6}>
-                                <label>{`Dancer ${i + 7}`}</label>
-                                <input type="text" placeholder={`Participant ${i + 7}`} style={{ marginBottom: 10 }} />
+                            <div className="admin-top__column top-column">
+                                {[...Array(6)].map((_, i) => (
+                                    <div className="top-column__block input-block" key={i + 6}>
+                                        <label className="top-column__title input-title">{`Dancer ${i + 7}`}</label>
+                                        <input className="top-column__input input-info" type="text"/>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                        {[...Array(4)].map((_, i) => (
-                            <div key={i + 12}>
-                                <label>{`Dancer ${i + 13}`}</label>
-                                <input type="text" placeholder={`Participant ${i + 13}`} style={{ marginBottom: 10 }} />
+                            <div className="admin-top__column top-column">
+                                {[...Array(4)].map((_, i) => (
+                                    <div className="top-column__block input-block"  key={i + 12}>
+                                        <label className="top-column__title input-title">{`Dancer ${i + 13}`}</label>
+                                        <input className="top-column__input input-info" type="text"/>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                        </div>
+                        <button className="admin-top__btn btn btn--black">SAVE</button>
                     </div>
-                </div>
-                <button>SAVE</button>
-            </div>
-        </Container>
+                    <div className="admin-top__logo"> </div>
+            </Container>
+        </div>
     )
 }
