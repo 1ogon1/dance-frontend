d889c9bc8f2d4cd850b6c64db25b4f6602f1236f
tireofthisplace <cristalaiz1337@gmail.com>
2023-05-31 13:17:13 +0300

    add useState and useEffect + fixed input

--- src/components/screen/ScreenBattleResult.js
+++ src/components/screen/ScreenBattleResult.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import  'components/screen/ScreenResult.css';
-import  'components/style/reset.css';
-import  'components/style/common.css';
+import React, { useState, useEffect } from "react";
+import "components/screen/ScreenResult.css";
+import "components/style/reset.css";
+import "components/style/common.css";
 
 export const ScreenBattleResult = () => {
     const [score1, setScore1] = useState(0);
@@ -17,30 +17,37 @@ export const ScreenBattleResult = () => {
         setScore2(Number(e.target.value));
     };
 
+    useEffect(() => {});
+
     return (
         <div className="result bg">
             <div className="result__container">
-
                 <div className="result__header">1/8 FINAL</div>
-
                 <div className="result__main main-result">
-
-                    <div className="main-result__logo">   </div>
-
+                    <div className="main-result__logo"> </div>
                     <div className="main-result__info result-info">
                         <div className="result-info__row info-row">
                             <div className="info-row__nickname name-screen">{nickname1}</div>
-                            <input className="info-row__input" type="text" placeholder="Score 1" value={score1} onChange={handleScoreChange1} />
+                            <input
+                                className="info-row__input"
+                                type="text"
+                                placeholder="Score 1"
+                                value={score1}
+                                onChange={handleScoreChange1}
+                            />
                         </div>
-                        
                         <div className="result-info__row info-row">
                             <div className="info-row__nickname name-screen">{nickname2}</div>
-                            <input className="info-row__input" type="text" placeholder="Score 2" value={score2} onChange={handleScoreChange2} />
+                            <input
+                                className="info-row__input"
+                                type="text"
+                                placeholder="Score 2"
+                                value={score2}
+                                onChange={handleScoreChange2}
+                            />
                         </div>
                     </div>
-
                 </div>
-
                 <button className="result__btn btn btn--orange"> BACK TO BATTLES </button>
             </div>
         </div>
