b69264cc93ec05abd48c485f63f4dad2370a5191
D1m30 <petrenko_dv@ukr.net>
2023-06-15 00:03:26 +0300

    1

--- src/components/screen/ScreenBattleResult.js
+++ src/components/screen/ScreenBattleResult.js
@@ -9,7 +9,7 @@ export const ScreenBattleResult = () => {
     const [score2, setScore2] = useState(0);
 
     const [judgeNickname1, setNickName1] = useState('TOLU');
-    const [judgeNickname2, setNickName2] = useState('NIKE');
+    const [judgeNickname2, setNickName2] = useState('NIKELODEON');
     const [judgeNickname3, setNickName3] = useState('ANKA')
 
     // Оценки для первого участника
@@ -57,91 +57,124 @@ export const ScreenBattleResult = () => {
     return (
         <div className="result bg">
             <div className="result__container">
-                <div className="result__main main-result">
-                    <div className="main-result__logo"> </div>
-                    <div className="main-result__info result-info">
-                        <div className="result-info__row info-row">
-                            <div className="info-row__judge"></div>
+
+{/* =============================Header=================================== */}
+            <div className="result__header header">
+                <div className="header__logo"> </div>
+                <div className="header__midle midle-block"> 
+                    <div className="midle-block__nickname nickname"> Nickelodeon</div>
+                    <div className="midle-block__row info-row">
                             <div className="info-row__judge">Музикальність</div>
                             <div className="info-row__judge">Техніка</div>
                             <div className="info-row__judge">Оригінальність</div>
                             <div className="info-row__judge">Подача</div>
                         </div>
-                        {/* Таблица для первого участника */}
-                        <div className="result-info__row info-row">
-                            <div className="info-row__nickname name-screen">
-                                {judgeNickname1}
-                            </div>
+                </div>
+                <button className="header__btn btn btn--orange">BACK TO BATTLES</button>
+            </div>
+{/* =============================Header(end)=================================== */}
+
+{/* =============================Section=================================== */}
+            <div className="result__section section section--top"> 
+
+            {/* -----------Блок: судья и результаты--------- */}
+            <div className="section__info info"> 
+
+                     {/* -----------Отдельный ряд Судья и Балы--------- */}
+                    <div className="info__block block-info"> 
+                      <div className="block-info__judge"> {judgeNickname1} </div>
+                      <div className="block-info__score">
                             <div className="info-row__score">{musicScore1Judge1}</div>
                             <div className="info-row__score">{techniqueScore1Judge1}</div>
                             <div className="info-row__score">{originalityScore1Judge1}</div>
                             <div className="info-row__score">{presentationScore1Judge1}</div>
-                        </div>
-                        <div className="result-info__row info-row">
-                            <div className="info-row__nickname name-screen">
-                                {judgeNickname2}
-                            </div>
+                      </div>
+                    </div>
+
+                    <div className="info__block block-info"> 
+                      <div className="block-info__judge"> {judgeNickname2} </div>
+                      <div className="block-info__score">
                             <div className="info-row__score">{musicScore1Judge2}</div>
                             <div className="info-row__score">{techniqueScore1Judge2}</div>
                             <div className="info-row__score">{originalityScore1Judge2}</div>
                             <div className="info-row__score">{presentationScore1Judge2}</div>
-                        </div>
-                        <div className="result-info__row info-row">
-                            <div className="info-row__nickname name-screen">
-                                {judgeNickname3}
-                            </div>
+                      </div>
+                    </div>
+
+                    <div className="info__block block-info"> 
+                      <div className="block-info__judge"> {judgeNickname3} </div>
+                      <div className="block-info__score">
                             <div className="info-row__score">{musicScore1Judge3}</div>
                             <div className="info-row__score">{techniqueScore1Judge3}</div>
                             <div className="info-row__score">{originalityScore1Judge3}</div>
                             <div className="info-row__score">{presentationScore1Judge3}</div>
-                        </div>
-                        <input
-                            className="info-row__input"
-                            type="text"
-                            placeholder="Score 1"
-                            value={score1}
-                            onChange={handleScoreChange1}
-                        />
-                        {/* Таблица для второго участника */}
-                        <div className="result-info__row info-row">
-                            <div className="info-row__nickname name-screen">
-                                {judgeNickname1}
-                            </div>
-                            <div className="info-row__score">{musicScore2Judge1}</div>
-                            <div className="info-row__score">{techniqueScore2Judge1}</div>
-                            <div className="info-row__score">{originalityScore2Judge1}</div>
-                            <div className="info-row__score">{presentationScore2Judge1}</div>
-                        </div>
-                        <div className="result-info__row info-row">
-                            <div className="info-row__nickname name-screen">
-                                {judgeNickname2}
-                            </div>
-                            <div className="info-row__score">{musicScore2Judge2}</div>
-                            <div className="info-row__score">{techniqueScore2Judge2}</div>
-                            <div className="info-row__score">{originalityScore2Judge2}</div>
-                            <div className="info-row__score">{presentationScore2Judge2}</div>
-                        </div>
-                        <div className="result-info__row info-row">
-                            <div className="info-row__nickname name-screen">
-                                {judgeNickname3}
-                            </div>
-                            <div className="info-row__score">{musicScore2Judge3}</div>
-                            <div className="info-row__score">{techniqueScore2Judge3}</div>
-                            <div className="info-row__score">{originalityScore2Judge3}</div>
-                            <div className="info-row__score">{presentationScore2Judge3}</div>
-                        </div>
-                        <input
-                            className="info-row__input"
-                            type="text"
-                            placeholder="Score 2"
-                            value={score2}
-                            onChange={handleScoreChange2}
-                        />
+                      </div>
                     </div>
+                     {/* -----------Отдельный ряд Судья и Балы(end)--------- */}
+
+            </div>
+             {/* -----------Блок: total--------- */}
+             <div className="section__total total">
+                <h2 className="total__text">Total:</h2>
+                <input className="total__result" type="text" placeholder="Score 1" value={score1} onChange={handleScoreChange1} />
                 </div>
-                <button className="result__btn btn btn--orange">
-                    BACK TO BATTLES
-                </button>
+
+            </div>
+
+{/* =============================Section(end)=================================== */}
+
+
+{/* =============================Section=================================== */}
+<div className="result__section section section--bottom"> 
+<div className="midle-block__nickname nickname"> Nickelodeon</div>
+<div className="section__box">
+    {/* -----------Блок: судья и результаты--------- */}
+    <div className="section__info info">
+    
+             {/* -----------Отдельный ряд Судья и Балы--------- */}
+            <div className="info__block block-info">
+              <div className="block-info__judge"> {judgeNickname1} </div>
+              <div className="block-info__score">
+                    <div className="info-row__score">{musicScore1Judge1}</div>
+                    <div className="info-row__score">{techniqueScore1Judge1}</div>
+                    <div className="info-row__score">{originalityScore1Judge1}</div>
+                    <div className="info-row__score">{presentationScore1Judge1}</div>
+              </div>
+            </div>
+    
+            <div className="info__block block-info">
+              <div className="block-info__judge"> {judgeNickname2} </div>
+              <div className="block-info__score">
+                    <div className="info-row__score">{musicScore1Judge2}</div>
+                    <div className="info-row__score">{techniqueScore1Judge2}</div>
+                    <div className="info-row__score">{originalityScore1Judge2}</div>
+                    <div className="info-row__score">{presentationScore1Judge2}</div>
+              </div>
+            </div>
+    
+            <div className="info__block block-info">
+              <div className="block-info__judge"> {judgeNickname3} </div>
+              <div className="block-info__score">
+                    <div className="info-row__score">{musicScore1Judge3}</div>
+                    <div className="info-row__score">{techniqueScore1Judge3}</div>
+                    <div className="info-row__score">{originalityScore1Judge3}</div>
+                    <div className="info-row__score">{presentationScore1Judge3}</div>
+              </div>
+            </div>
+             {/* -----------Отдельный ряд Судья и Балы(end)--------- */}
+    
+    </div>
+     {/* -----------Блок: total--------- */}
+     <div className="section__total total">
+        <h2 className="total__text">Total:</h2>
+        <input className="total__result" type="text" placeholder="Score 1" value={score1} onChange={handleScoreChange1} />
+        </div>
+</div>
+
+</div>
+
+{/* =============================Section(end)=================================== */}
+              
             </div>
         </div>
     );
